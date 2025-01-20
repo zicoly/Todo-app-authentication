ff716f57b6ebe24d0e5c8e641a72648f618caccc
Zion <omojolazion@gmail.com>
2025-01-20 17:43:59 +0100

    Initial commit

---
+++ Todo-app-authentication/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
