cd1b90dc3bce295e8a8397f4bf2f9d44caf90734
Zion <omojolazion@gmail.com>
2025-01-20 18:25:50 +0100

    initial commit#

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-500">
       <h1 className="text-white text-4xl font-bold">
-        Tailwind CSS is Working!
+        Todo App
       </h1>
     </div>
   );
